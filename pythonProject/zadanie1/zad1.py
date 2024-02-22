9b023b74a04f92a54b9e4a3b92e897f4b4027e93
Syllgiusz <u3_miklaw_lbn@technischools.com>
2024-02-22 09:31:24 +0100

    Dodano pliki tekstowe oraz zaktualizowano skrypt

--- pythonProject/zadanie1/zad1.py
+++ pythonProject/zadanie1/zad1.py
@@ -1,2 +1,11 @@
-def sumFileNumbers():
-    pass
+def sumFileNumbers(file):
+    with open(file) as f:
+        numbers = f.read()
+    numbers = numbers.split('; ')
+
+    sum = 0
+    for i in numbers:
+        number = int(i)
+        if number <= 10 and number >= -10:
+            sum += number
+    return sum
--- testki/test_zad1.py
+++ testki/test_zad1.py
@@ -4,10 +4,10 @@ from pythonProject.zadanie1.zad1 import sumFileNumbers
 
 class Test(TestCase):
     def test_sum_file_numbers(self):
-        self.assertEqual(sumFileNumbers("file1"), 10)
+        self.assertEqual(sumFileNumbers("../pythonProject/zadanie1/file1"), 10)
 
     def empty_file(self):
-        self.assertEqual(sumFileNumbers("file2"), 0)
+        self.assertEqual(sumFileNumbers("../pythonProject/zadanie1/file2"), 0)
 
     def file_doesnt_exist(self):
         with self.assertRaises(FileNotFoundError):
